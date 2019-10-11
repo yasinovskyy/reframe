7df0a9a7f87e411f80348b6cf32f07bead5941f7
Anna Ruble <rublan01@annas-mbp.lc.luther.edu>
2019-10-11 13:43:01 -0500

    adding three test cases, project done except for docs

---
+++ tests/test_join.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import pytest
+from reframe import Relation
+
--- tests/test_project.py
+++ tests/test_project.py
@@ -4,18 +4,22 @@ from reframe import Relation
 
 @pytest.fixture
 def r():
-    data_real = {"name": ["Alice", "Bob", "Carol"], "age": [86, 4, 37]}
+    data_real = {"firstName": ["Alice", "Bob", "Carol"], "lastName": ["Smith", "Johnson", "Williams"], "age": [86, 4, 37]}
     df = pd.DataFrame(data=data_real)
     return Relation(df)
 
-def test_foo(r):
-    r = r.project(["name"])
-    data_expected = {"name": ["Alice", "Bob", "Carol"]}
-    df_expected = pd.DataFrame(data=data_expected)
-    r_expected = Relation(df_expected)
-    assert r.equals(r_expected)
-
-def test_foo_2(r):
-    r = r.project(["name"])
-    r_expected_2 = Relation("tests/test_project_expected.csv", sep="|")
+def test_project_expected1(r):
+    r = r.project(["firstName"])
+    r_expected_2 = Relation("tests/test_project_expected1.csv", sep="|")
     assert r.equals(r_expected_2)
+
+def test_project_expected2(r):
+    r = r.project(["age"])
+    r_expected_2 = Relation("tests/test_project_expected2.csv", sep="|")
+    assert r.equals(r_expected_2)
+
+def test_project_expected3(r):
+    r = r.project(["lastName"])
+    r_expected_2 = Relation("tests/test_project_expected3.csv", sep="|")
+    assert r.equals(r_expected_2)
+
