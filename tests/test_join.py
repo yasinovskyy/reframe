577d117a66093fe6b75a5ab886c824c5f91b51f1
Anna Ruble <rublan01@annas-mbp.lc.luther.edu>
2019-10-17 19:55:54 -0500

    all tests passing

--- tests/test_join.py
+++ tests/test_join.py
@@ -4,31 +4,44 @@ from reframe import Relation
 
 @pytest.fixture
 def makePerson():
-    data1 = {"id": [0, 1, 2], "firstName": ["Alice", "Bob", "Carol"], "lastName": ["Smith", "Johnson", "Williams"], "age": [86, 4, 37]}
-    data2 = {"id": [0, 1, 2], "street": ["102 Day St", "5401 95th Ave", "810 Mulberry Ln"], "city": ["Atlanta", "Des Moines", "Eden Prairie"], "state": ["GA", "IA", "MN"]}
+    #data1 = {"id": [0, 1, 2], "firstName": ["Alice", "Bob", "Carol"], "lastName": ["Smith", "Johnson", "Williams"], "age": [86, 24, 37]}
+    data1 = {"id": [0, 1], "firstName": ["Alice", "Bob"], "lastName": ["Smith", "Johnson"]}
     person = pd.DataFrame(data=data1)
-    address = pd.DataFrame(data=data2)
-    return Relation(person, address)
-'''
+    return Relation(person)
+
 @pytest.fixture
 def makeAddress():
-    data2 = {"id": [0, 1, 2], "street": ["102 Day St", "5401 95th Ave", "810 Mulberry Ln"], "city": ["Atlanta", "Des Moines", "Eden Prairie"], "state": ["GA", "IA", "MN"]}
+    #data2 = {"id": [0, 1, 2], "street": ["102 Day St", "5401 95th Ave", "810 Mulberry Ln"], "city": ["Atlanta", "Des Moines", "Eden Prairie"], "state": ["GA", "IA", "MN"]}
+    data2 = {"id": [0, 1], "city": ["Atlanta", "Des Moines"], "state": ["GA", "IA"]}
     address = pd.DataFrame(data=data2)
     return Relation(address)
-'''
-def test_join_expected1(makePerson):
-    personInfo = makePerson.query("id").njoin(makePerson.query("id"))
+
+@pytest.fixture
+def makeAge():
+    data3 = {"id": [0,1], "age": [86, 24]}
+    age = pd.DataFrame(data=data3)
+    return Relation(age)
+
+@pytest.fixture
+def makeContact():
+    data4 = {"id": [0,1], "email": ["thesmiths@homestead.com", "bobby@site.com"], "phone": ["555-107-1234", "868-402-7539"]}
+    contact = pd.DataFrame(data=data4)
+    return Relation(contact)
+
+def test_join_expected1(makePerson, makeAddress):
+    personInfo = makePerson.njoin(makeAddress)
     personInfo_expected = Relation("tests/test_join_expected1.csv", sep="|")
     assert personInfo.equals(personInfo_expected)
 
-'''
-def test_join_expected2(r):
-    r = r.join(["age"])
-    r_expected_2 = Relation("tests/test_join_expected2.csv", sep="|")
-    assert r.equals(r_expected_2)
+def test_join_expected2(makePerson, makeAge):
+    personInfo = makePerson.njoin(makeAge)
+    personInfo_expected = Relation("tests/test_join_expected2.csv", sep="|")
+    assert personInfo.equals(personInfo_expected)
 
-def test_join_expected3(r):
-    r = r.join(["lastName"])
-    r_expected_2 = Relation("tests/test_join_expected3.csv", sep="|")
-    assert r.equals(r_expected_2)
-'''
+def test_join_expected3(makePerson, makeContact):
+    personInfo = makePerson.njoin(makeContact)
+    print(personInfo)
+    personInfo_expected = Relation("tests/test_join_expected3.csv", sep="|")
+    print(personInfo_expected)
+    assert personInfo.equals(personInfo_expected)
+
